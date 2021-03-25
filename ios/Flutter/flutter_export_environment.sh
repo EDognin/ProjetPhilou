8968e67bdf97ab87d2160b9866caa061dd2e4232
Edouard Dognin <edouard@dognin.name>
2021-03-25 16:24:33 -0400

    initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/edouard/Documents/UdemyFlutter/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/edouard/Documents/UdemyFlutter/Projects/udemy5_xylophone"
+export "FLUTTER_TARGET=/Users/edouard/Documents/UdemyFlutter/Projects/udemy5_xylophone/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/edouard/Documents/UdemyFlutter/Projects/udemy5_xylophone/.dart_tool/package_config.json"
