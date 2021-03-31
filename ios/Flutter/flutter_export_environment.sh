6911c2665b7abbbb579c84b1fb1bc4c88c257e99
Edouard Dognin <edouard@dognin.name>
2021-03-31 12:45:55 -0400

    initial commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/edouard/Documents/UdemyFlutter/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/edouard/Documents/UdemyFlutter/Projects/udemy5_xylophone"
-export "FLUTTER_TARGET=/Users/edouard/Documents/UdemyFlutter/Projects/udemy5_xylophone/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/edouard/Documents/UdemyFlutter/Projects/ProjetPhilou"
+export "FLUTTER_TARGET=/Users/edouard/Documents/UdemyFlutter/Projects/ProjetPhilou/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
@@ -11,4 +11,4 @@ export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/edouard/Documents/UdemyFlutter/Projects/udemy5_xylophone/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/edouard/Documents/UdemyFlutter/Projects/ProjetPhilou/.dart_tool/package_config.json"
